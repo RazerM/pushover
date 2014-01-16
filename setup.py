8e20b3ba1ab44d0519cb8eaf2bddfa350502090f
Frazer McLean <frazergmclean@gmail.com>
2014-01-16 05:21:12 +0000

    Change setup.py to my fork

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@
 from distutils.core import setup
 
 setup(
-        name='pushover',
-        version='0.1',
-        url='https://github.com/Wyattjoh/pushover',
-        py_modules=['pushover'],
-        scripts=['pushover'],
-        author='Wyatt Johnson',
-        author_email='wyattjoh@gmail.com',
-        )
+    name='pushover',
+    version='0.1',
+    url='https://github.com/RazerM/pushover',
+    py_modules=['pushover'],
+    scripts=['pushover'],
+    author='Frazer McLean',
+    author_email='frazergmclean@gmail.com',
+)
